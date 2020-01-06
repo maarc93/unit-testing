dbfdfa8ff4e12ee0d49e8ba7244532ffd9e70db0
Marcis Zoldners <marcis.zoldners@gmail.com>
2020-01-06 23:03:32 +0200

    Testing a Collection class

---
+++ tests/unit/UserTest.php
@@ -0,0 +1,64 @@
+<?php
+
+class UserTest extends \PHPUnit\Framework\TestCase
+{
+    protected $user;
+
+    public function setUp(): void
+    {
+        $this->user = new \App\Models\User;
+    }
+
+    public function testThatWeCanGetTheFirstName()
+    {
+        $this->user->setFirstName('Billy');
+
+        $this->assertEquals($this->user->getFirstName(), 'Billy');
+    }
+
+    public function testThatWeCanGetTheLastName()
+    {
+        $this->user->setLastName('Garrett');
+
+        $this->assertEquals($this->user->getLastName(), 'Garrett');
+    }
+
+    public function testFullNameIsReturned()
+    {
+        $this->user->setFirstName('Billy');
+        $this->user->setLastName('Garrett');
+
+        $this->assertEquals($this->user->getFullName(), 'Billy Garrett');
+    }
+
+    public function testFirstAndLastNameAreTrimmed()
+    {
+        $this->user->setFirstName('Billy   ');
+        $this->user->setLastName('   Garrett');
+
+        $this->assertEquals($this->user->getFirstName(), 'Billy');
+        $this->assertEquals($this->user->getLastName(), 'Garrett');
+    }
+
+    public function testEmailAddressCanBeSet()
+    {
+        $this->user->setEmail('billy@codecourse.com');
+    
+        $this->assertEquals($this->user->getEmail(), 'billy@codecourse.com');
+    }
+
+    public function testEmailVariablesContainCorrectValues()
+    {
+        $this->user->setFirstName('Billy');
+        $this->user->setLastName('Garrett');
+        $this->user->setEmail('billy@codecourse.com');
+
+        $emailVariables = $this->user->getEmailVariables();
+
+        $this->assertArrayHasKey('full_name', $emailVariables);
+        $this->assertArrayHasKey('email', $emailVariables);
+
+        $this->assertEquals($emailVariables['full_name'], 'Billy Garrett');
+        $this->assertEquals($emailVariables['email'], 'billy@codecourse.com');
+    }
+}
